8752d53204eeb3b2c350f506eefd781c8e125bd1
JJ Kanu <jjkanu20@gmail.com>
2020-10-05 17:31:59 -0400

    Added Add-To-Cart Functionality with Hard-Coded Items

--- React/calculator/src/components/calculator.jsx
+++ React/calculator/src/components/calculator.jsx
@@ -4,6 +4,13 @@ import Grid from "@material-ui/core/Grid";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import { FixedSizeList } from "react-window";
+
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import Box from "@material-ui/core/Box";
@@ -14,21 +21,60 @@ class Calculator extends Component {
     number_of_items: 0,
     subtotal: 0.0,
     bags_used: 0,
+    catalog: [
+      { name: "Apple", price: 0.67 },
+      { name: "Banana", price: 0.33 },
+      { name: "Cake Mix", price: 2.99 },
+      { name: "Syrup", price: 3.99 },
+    ],
+    cart: [],
   };
 
-  //   constructor() {
-  //     super();
-  //     this.bagIncrement = this.bagIncrement.bind(this);
-  //     this.bagDecrement = this.bagDecrement.bind(this);
-  //   }
-
-  bagIncrement = () => {
+  /*
+  addItemToCatalog = () => {
     //console.log("Increment Clicked");
     this.setState({ bags_used: this.state.bags_used + 1 });
     this.setState({ subtotal: this.state.subtotal + 0.05 });
   };
-  bagDecrement = () => {
+
+  itemIncrement = () => {
+    cart: [
+        ...this.state.cart,
+        { name: itemName, price: itemPrice, quantity: 1 },
+      ],
+    this.setState({ subtotal: this.state.subtotal + 0.05 });
+  };
+  itemDecrement = () => {
     //console.log("Increment Clicked");
+    if (this.state.bags_used >= 1) {
+      this.setState({ bags_used: this.state.bags_used - 1 });
+      this.setState({ subtotal: this.state.subtotal - 0.05 });
+    };
+
+
+    removeItem = () => {
+        //console.log("Increment Clicked");
+        if (this.state.bags_used >= 1) {
+        this.setState({ bags_used: this.state.bags_used - 1 });
+        this.setState({ subtotal: this.state.subtotal - 0.05 });
+        };
+  */
+
+  addToCart = (itemName, itemPrice) => {
+    this.setState({
+      cart: [
+        ...this.state.cart,
+        { name: itemName, price: itemPrice, quantity: 1 },
+      ],
+    });
+    this.setState({ subtotal: this.state.subtotal + itemPrice });
+  };
+
+  bagIncrement = () => {
+    this.setState({ bags_used: this.state.bags_used + 1 });
+    this.setState({ subtotal: this.state.subtotal + 0.05 });
+  };
+  bagDecrement = () => {
     if (this.state.bags_used >= 1) {
       this.setState({ bags_used: this.state.bags_used - 1 });
       this.setState({ subtotal: this.state.subtotal - 0.05 });
@@ -70,13 +116,26 @@ class Calculator extends Component {
                 Create New Item
               </Button>
             </form>
+
+            {/*Catalog*/}
             <Box
               style={{ marginTop: "0.8rem" }}
               Topwidth={600}
               height={370}
               border={1}
             >
-              List of Items go here
+              <List component="nav">
+                {this.state.catalog.map((item) => (
+                  <ListItem
+                    button
+                    onClick={(e) => this.addToCart(item.name, item.price)}
+                  >
+                    {" "}
+                    {item.name}
+                    <text style={{ marginLeft: "auto" }}>${item.price}</text>
+                  </ListItem>
+                ))}
+              </List>
             </Box>
           </Grid>
 
@@ -86,12 +145,25 @@ class Calculator extends Component {
               <Grid item>
                 <h3>Shopping Cart</h3>
               </Grid>
+
+              {/*Shopping Cart*/}
               <Grid item xs={12} height="20rem">
-                <Box width={350} height={250} border={1}>
-                  {this.formatNumberOfItems()}
+                <Box width={390} height={250} border={1}>
+                  <List component="nav">
+                    {this.state.cart.map((item) => (
+                      <ListItem button>
+                        {" "}
+                        {item.name}
+                        <text style={{ marginLeft: "auto" }}>
+                          ${item.price} x{item.quantity}
+                        </text>
+                      </ListItem>
+                    ))}
+                  </List>
                 </Box>
               </Grid>
 
+              {/*Add Bags*/}
               <Grid item>
                 <text style={{ fontWeight: "bold" }}>
                   Number of Bags Used:{" "}
@@ -158,8 +230,26 @@ class Calculator extends Component {
 
   formatNumberOfItems() {
     const { number_of_items } = this.state;
-    return number_of_items == 0 ? "Empty Cart" : number_of_items;
+    return number_of_items === 0 ? "Empty Cart" : number_of_items;
   }
+
+  /*
+
+  renderRow(props) {
+    const { index, style } = props;
+  
+    return (
+      <ListItem button style={style} key={index}>
+        <ListItemText primary={`Item ${index + 1}`} />
+      </ListItem>
+    );
+  }
+  
+  renderRow.propTypes = {
+    index: PropTypes.number.isRequired,
+    style: PropTypes.object.isRequired,
+  };
+  */
 }
 
 export default Calculator;
